1324cf4dc8218152fc96ff69e9b3cf9eb3f36196
Sephirothzc <Sephirothzc@gmail.com>
2015-09-23 18:25:57 +0800

    摄像机控制部分基本完成

--- Source/ProjectC/Private/PlatformCameraSpring.cpp
+++ Source/ProjectC/Private/PlatformCameraSpring.cpp
@@ -19,6 +19,17 @@ UPlatformCameraSpring::UPlatformCameraSpring(const FObjectInitializer& ObjectIni
 	CameraChangeLimit = FVector(0.f, 0.f, 0.f);
 	CameraFollowLimit = FVector(0.f, 0.f, 0.f);
 	m_is_attach_follow_point = true;
+
+	CameraLerpSpeed = 5.f;
+	CameraLerpMaxDistance = 50;
+	CameraLerpTolerance = 5.f;
+
+	m_cur_camera_state = PLATFORM_CAMERA_STATE_NONE;
+}
+
+void UPlatformCameraSpring::BeginPlay() {
+	m_pre_camera_location = GetComponentLocation() + TargetOffset;
+	ChangeCameraState(PLATFORM_CAMERA_STATE_RIGHT);
 }
 
 void UPlatformCameraSpring::OnTurnRound(FVector cur_forward)
@@ -40,63 +51,30 @@ void UPlatformCameraSpring::UpdateDesiredArmLocation(bool bDoTrace, bool bDoLoca
 void UPlatformCameraSpring::UpdatePlatformCameraLerp(bool bDoPlatformLerp, float DeltaTime)
 {
 	FRotator DesiredRot = GetComponentRotation();
-
 	FVector ArmOrigin = GetComponentLocation() + TargetOffset;
-
-	FVector ActorOrigin = GetComponentLocation();
-
 	FVector DesiredLoc = ArmOrigin;
-
-	FVector FollowPoint;
-	FVector LimitPoint = DesiredLoc + CameraChangeLimit;
-
-	if (m_is_attach_follow_point) {
-		FollowPoint = DesiredLoc + CameraFollowLimit;
-	}
-	else {
-		FollowPoint = PreviousDesiredLoc + CameraFollowLimit;
-	}
+	
+	FVector LimitPoint = m_pre_camera_location + CameraChangeLimit;
+	FVector FollowPoint = m_pre_camera_location + CameraFollowLimit;
 
 	if (bEnablePlatformCameraLerp) {
-		if (m_is_cur_owner_fowrard_left == m_is_forward_left) {
-			if (FollowPoint.X <= ActorOrigin.X + 1.f) {
-				m_is_attach_follow_point = true;
-			}
-			else {
-				m_is_attach_follow_point = false;
-				DesiredLoc.X = PreviousDesiredLoc.X;
-			}
-		}
-		else {
-			if (LimitPoint.X > ActorOrigin.X + 1) {
-				TargetOffset = -TargetOffset;
-				m_is_forward_left = !m_is_forward_left;
-			}
-			else {
-				m_is_attach_follow_point = false;
-				DesiredLoc.X = PreviousDesiredLoc.X;
-			}
-		}
+		DesiredLoc.X = CalcCameraPos(FollowPoint, LimitPoint, DeltaTime);
 	}
-
-
 
 #if !(UE_BUILD_SHIPPING || UE_BUILD_TEST)
 	if (bDrawPlatformCameraLerpMarkers)
 	{
+		FVector ActorOrigin = GetComponentLocation();
 		DrawDebugSphere(GetWorld(), ActorOrigin, 5.f, 8, FColor::Green);
-		DrawDebugSphere(GetWorld(), FollowPoint, 5.f, 8, FColor::Yellow);
+		//DrawDebugSphere(GetWorld(), FollowPoint, 5.f, 8, FColor::Yellow);
+		//DrawDebugSphere(GetWorld(), LimitPoint, 5.f, 8, FColor::Red);
 
-		DrawDebugSphere(GetWorld(), LimitPoint, 5.f, 8, FColor::Red);
-		
-
-		//const FVector ToOrigin = ArmOrigin - DesiredLoc;
-		//DrawDebugDirectionalArrow(GetWorld(), DesiredLoc, DesiredLoc + ToOrigin * 0.5f, 7.5f, bClampedDist ? FColor::Red : FColor::Green);
-		//DrawDebugDirectionalArrow(GetWorld(), DesiredLoc + ToOrigin * 0.5f, ArmOrigin, 7.5f, bClampedDist ? FColor::Red : FColor::Green);
+		DrawDebugLine(GetWorld(), FVector(LimitPoint.X, LimitPoint.Y, -10000.f), FVector(LimitPoint.X, LimitPoint.Y, 10000.f), FColor::Red);
+		DrawDebugLine(GetWorld(), FVector(FollowPoint.X, LimitPoint.Y, -10000.f), FVector(FollowPoint.X, LimitPoint.Y, 10000.f), FColor::Yellow);
 	}
 #endif
 	
-	PreviousDesiredLoc = DesiredLoc;
+	m_pre_camera_location = DesiredLoc;
 
 	// Now offset camera position back along our rotation
 	DesiredLoc -= DesiredRot.Vector() * TargetArmLength;
@@ -116,3 +94,141 @@ void UPlatformCameraSpring::UpdatePlatformCameraLerp(bool bDoPlatformLerp, float
 	UpdateChildTransforms();
 }
 
+void UPlatformCameraSpring::EnterCameraState(PLATFORM_CAMERA_STATE next_state) {
+	switch (next_state)
+	{
+	case UPlatformCameraSpring::PLATFORM_CAMERA_STATE_LEFT:
+		m_is_forward_left = false;
+		break;
+
+	case UPlatformCameraSpring::PLATFORM_CAMERA_STATE_RIGHT:
+		m_is_forward_left = true;
+		break;
+
+	case UPlatformCameraSpring::PLATFORM_CAMERA_STATE_MOVE_LEFT:
+		TargetOffset = -TargetOffset;
+		CameraChangeLimit = -CameraChangeLimit;
+		CameraFollowLimit = -CameraFollowLimit;
+		m_camera_move_nearly_attach = false;
+		break;
+
+	case UPlatformCameraSpring::PLATFORM_CAMERA_STATE_MOVE_RIGHT:
+		TargetOffset = -TargetOffset;
+		CameraChangeLimit = -CameraChangeLimit;
+		CameraFollowLimit = -CameraFollowLimit;
+		m_camera_move_nearly_attach = false;
+		break;
+
+	default:
+		break;
+	}
+	m_cur_camera_state = next_state;
+}
+
+void UPlatformCameraSpring::ExitCameraState(PLATFORM_CAMERA_STATE cur_state) {
+	switch (cur_state)
+	{
+	case UPlatformCameraSpring::PLATFORM_CAMERA_STATE_LEFT:
+		
+		break;
+
+	case UPlatformCameraSpring::PLATFORM_CAMERA_STATE_RIGHT:
+		
+		break;
+
+	case UPlatformCameraSpring::PLATFORM_CAMERA_STATE_MOVE_LEFT:
+		break;
+
+	case UPlatformCameraSpring::PLATFORM_CAMERA_STATE_MOVE_RIGHT:
+		break;
+
+	default:
+		break;
+	}
+}
+
+void UPlatformCameraSpring::ChangeCameraState(PLATFORM_CAMERA_STATE next_state) {
+	ExitCameraState(m_cur_camera_state);
+	EnterCameraState(next_state);
+}
+
+float UPlatformCameraSpring::CalcCameraPos(const FVector& follow_point, const FVector& limit_point, float delta_seconds) {
+	float ActorLocationX = GetComponentLocation().X;
+	float CameraLocationX = (GetComponentLocation() + TargetOffset).X;
+
+	switch (m_cur_camera_state)
+	{
+	case UPlatformCameraSpring::PLATFORM_CAMERA_STATE_LEFT: 
+	case UPlatformCameraSpring::PLATFORM_CAMERA_STATE_RIGHT: {
+		bool follow_check = false;
+		bool limit_check = false;
+		if (m_is_forward_left) {
+			follow_check = follow_point.X <= ActorLocationX;
+			limit_check = limit_point.X > ActorLocationX;
+		}
+		else {
+			follow_check = follow_point.X >= ActorLocationX;
+			limit_check = limit_point.X < ActorLocationX;
+		}
+		if (m_is_cur_owner_fowrard_left == m_is_forward_left) {
+			if (follow_check) {
+				m_is_attach_follow_point = true;
+			}
+			else {
+				m_is_attach_follow_point = false;
+				CameraLocationX = m_pre_camera_location.X;
+			}
+		}
+		else {
+			if (limit_check) {
+				if (m_is_forward_left) {
+					ChangeCameraState(PLATFORM_CAMERA_STATE_MOVE_LEFT);
+				}
+				else {
+					ChangeCameraState(PLATFORM_CAMERA_STATE_MOVE_RIGHT);
+				}
+				CameraLocationX = m_pre_camera_location.X;
+			}
+			else {
+				m_is_attach_follow_point = false;
+				CameraLocationX = m_pre_camera_location.X;
+			}
+		}
+		break;
+	}
+
+	case UPlatformCameraSpring::PLATFORM_CAMERA_STATE_MOVE_LEFT:
+	case UPlatformCameraSpring::PLATFORM_CAMERA_STATE_MOVE_RIGHT: {
+		FVector target_location = GetComponentLocation() + TargetOffset;
+		CameraLocationX = FMath::FInterpTo(m_pre_camera_location.X, target_location.X, delta_seconds, CameraLerpSpeed);
+
+		const float FromOriginX = CameraLocationX - target_location.X;
+		if (!m_camera_move_nearly_attach) {
+			if (FromOriginX < CameraLerpMaxDistance) {
+				m_camera_move_nearly_attach = true;
+			}
+		}
+		else {
+			if (FromOriginX > CameraLerpMaxDistance) {
+				CameraLocationX = target_location.X + CameraLerpMaxDistance;
+			}
+		}
+
+		if (FMath::Abs(target_location.X - CameraLocationX) < CameraLerpTolerance) {
+			if (m_cur_camera_state == PLATFORM_CAMERA_STATE_MOVE_LEFT) {
+				ChangeCameraState(PLATFORM_CAMERA_STATE_LEFT);
+			}
+			if (m_cur_camera_state == PLATFORM_CAMERA_STATE_MOVE_RIGHT) {
+				ChangeCameraState(PLATFORM_CAMERA_STATE_RIGHT);
+			}
+		}
+
+		break;
+	}
+
+	default:
+		break;
+	}
+
+	return CameraLocationX;
+}
--- Source/ProjectC/Public/PlatformCameraSpring.h
+++ Source/ProjectC/Public/PlatformCameraSpring.h
@@ -28,6 +28,15 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = PlatformCamera, meta = (editcondition = "bEnablePlatformCameraLerp"))
 	FVector CameraFollowLimit;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = PlatformCamera, meta = (editcondition = "bEnablePlatformCameraLerp"))
+	float CameraLerpSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = PlatformCamera, meta = (editcondition = "bEnablePlatformCameraLerp"))
+	int32 CameraLerpMaxDistance;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = PlatformCamera, meta = (editcondition = "bEnablePlatformCameraLerp"))
+	float CameraLerpTolerance;
+	
 
 	UFUNCTION(BlueprintCallable, Category = PlatformCamera)
 	void OnTurnRound(FVector cur_forward);
@@ -37,9 +46,30 @@ public:
 
 	virtual void UpdatePlatformCameraLerp(bool bDoPlatformLerp, float DeltaTime);
 
+	virtual void BeginPlay();
+
 private:
+	enum PLATFORM_CAMERA_STATE {
+		PLATFORM_CAMERA_STATE_NONE = 0,
+		PLATFORM_CAMERA_STATE_LEFT,
+		PLATFORM_CAMERA_STATE_RIGHT,
+		PLATFORM_CAMERA_STATE_MOVE_LEFT,
+		PLATFORM_CAMERA_STATE_MOVE_RIGHT,
+		PLATFORM_CAMERA_STATE_MOVE_MAX,
+	};
+
+	void EnterCameraState(PLATFORM_CAMERA_STATE next_state);
+	void ExitCameraState(PLATFORM_CAMERA_STATE cur_state);
+	void ChangeCameraState(PLATFORM_CAMERA_STATE next_state);
+	float CalcCameraPos(const FVector& follow_point, const FVector& limit_point, float delta_seconds);
+
+	PLATFORM_CAMERA_STATE m_cur_camera_state;
 	bool m_is_forward_left;
 	bool m_is_cur_owner_fowrard_left;
 
 	bool m_is_attach_follow_point;
+
+	bool m_camera_move_nearly_attach;
+
+	FVector m_pre_camera_location;
 };
